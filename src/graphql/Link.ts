26a5ce72fca769eccf7dc6d3a1ce1a8004d8fc04
davi38 <davi4alves@gmail.com>
2022-09-09 22:34:14 -0300

    link finished

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,5 @@
-import { objectType } from "nexus";
+import { extendType, objectType, nonNull, stringArg } from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
     name: "Link", // 1
@@ -6,5 +7,57 @@ export const Link = objectType({
         t.nonNull.int("id"); // 3
         t.nonNull.string("description"); // 4
         t.nonNull.string("url"); // 5
+    },
+});
+
+let links: NexusGenObjects["Link"][] = [
+    {
+        id: 1,
+        url: "www.howtographql.com",
+        description: "Fullstack tutorial for GraphQL",
+    },
+    {
+        id: 2,
+        url: "graphql.org",
+        description: "GraphQL official website",
+    },
+
+];
+
+export const LinkQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("feed", {
+            type: "Link",
+            resolve(parent, args, context, info) {
+                return links;
+            }
+        })
+    }
+})
+
+export const LinkMutation = extendType({
+    type: "Mutation",
+    definition(t){
+        t.nonNull.field("post", {
+            type: "Link",
+            args: {
+                description: nonNull(stringArg()),
+                url: nonNull(stringArg()),
+            },
+
+            resolve(parent,args,context){
+                const {description, url} = args;
+
+                let idCount = links.length + 1;
+                const link = {
+                    id: idCount,
+                    description: description,
+                    url: url,
+                };
+                links.push(link);
+                return link;
+            },
+        });
     },
 });
