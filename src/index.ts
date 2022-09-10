7b8b4ec16c2b9fe4e55d5f7fe671ae6904c1d359
davi38 <davi4alves@gmail.com>
2022-09-09 21:58:55 -0300

    feat: add Link.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from "apollo-server";
 import { schema } from './schema'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 
-export const server = new ApolloServer({ schema, });
+export const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+});
 const port = 3000;
 server.listen({ port }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀 The Server ready at ${url}`);
 });
